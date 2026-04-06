852980a84fed53dee847692eb6335f05cb0d34eb
Connor Barrett <wafflefx05@gmail.com>
2026-04-06 16:16:05 -0400

    Setup for Damage and Tool Scripts (will be finalized later cus my laptop)

---
+++ Assets/Scripts/ToolUse.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class toolUse : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //raycast with box
+    }
+    void FixedUpdate()
+    {
+        //same thing with jump, but make it tool use with tag "breakable"
+    }
+}
