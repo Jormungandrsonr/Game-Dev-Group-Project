072444f2e018182d7145b09be6bac8b144312533
Connor Barrett <wafflefx05@gmail.com>
2026-04-07 19:36:03 -0400

    Need to work on getting the RayCast to move based on the direction the player is looking.

--- Assets/Scripts/ToolUse.cs
+++ Assets/Scripts/ToolUse.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 
-public class toolUse : MonoBehaviour
+public class toolUse : BasicMovement
 {
     Rigidbody2D rb2d;
+    BasicMovement move;
+    Collider2D collide;
 
 
-    // Update is called once per frame
+    
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        move = GetComponent<BasicMovement>();
+        collide = GetComponent<Collider2D>();
     }
     void Update()
     {
-        
+        bool breakableInReach = IsFacingBreakable();
+        if(breakableInReach)
+        {
+            Debug.Log("In reach.");
+        } 
+        else
+        {
+            Debug.Log("Out of Reach");
+        }
         //raycast with box, check if tag, else have a question mark appear
         //simple tags on breakable objects/ocean spots
     }
