3402a84049132c393cf477f8732f262ecd9e727f
Connor Barrett <wafflefx05@gmail.com>
2026-04-07 13:30:47 -0400

    Type

--- Assets/Scripts/ToolUse.cs
+++ Assets/Scripts/ToolUse.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 
 public class toolUse : MonoBehaviour
 {
+    Rigidbody2D rb2d;
+
+
     // Update is called once per frame
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        //raycast with box
+        
+        //raycast with box, check if tag, else have a question mark appear
+        //simple tags on breakable objects/ocean spots
     }
     void FixedUpdate()
     {
