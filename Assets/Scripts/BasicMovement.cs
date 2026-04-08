b5515d7151c839d135d5c4b0c35bd4d6950a5067
Connor Barrett <wafflefx05@gmail.com>
2026-04-08 15:20:59 -0400

    raycast is now based on direction. time for boxcast debug/not hitting the player

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -6,7 +6,8 @@ public class BasicMovement : MonoBehaviour
     public Vector2 currentMovementDirection = Vector2.zero;
     public Vector2 boxSize = new Vector2(0.08f,0.001f);
     public float breakableCheckDistance = 0.01f;
-    public float breakableCheckOffset = 0.12f;
+    public float breakableCheckOffsetX = 0.1f;
+    public float breakableCheckOffsetY = 0.2f;
 
     public void SetLastMove(Vector2 movement)
     {
@@ -24,29 +25,7 @@ public class BasicMovement : MonoBehaviour
             currentMovementDirection = Vector2.zero;
         }
     }
-    public bool IsFacingBreakable()
-    {
-        float lastPositionOffsetX = lastMovementDirection.x * breakableCheckOffset;
-        float lastPositionOffsetY = lastMovementDirection.y * breakableCheckOffset;
-        Vector2 rayStart = new Vector2((transform.position.x + lastPositionOffsetX), (transform.position.y + lastPositionOffsetY));    
-
-        RaycastHit2D hit = Physics2D.BoxCast(rayStart, boxSize, 0, lastMovementDirection, breakableCheckDistance);
-
-        bool grounded = hit.collider != null;
-        grounded = hit.collider.tag != "Player";
-        Debug.DrawRay(rayStart, lastMovementDirection*breakableCheckDistance, grounded ? Color.blue: Color.red);
-        
-        if(grounded)
-        {
-            Debug.DrawRay(rayStart, lastMovementDirection*breakableCheckDistance,Color.red);
-        }
-        else
-        {
-            Debug.DrawRay(rayStart, lastMovementDirection*breakableCheckDistance, Color.green);
-        }
-        
-        return grounded;
-    }
+    
     /*
     public bool IsGrounded()
     {
--- Assets/Scripts/ToolUse.cs
+++ Assets/Scripts/ToolUse.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class toolUse : BasicMovement
+public class toolUse : MonoBehaviour
 {
     Rigidbody2D rb2d;
     BasicMovement move;
@@ -23,7 +23,7 @@ public class toolUse : BasicMovement
         } 
         else
         {
-            Debug.Log("Out of Reach");
+            //Debug.Log("Out of Reach");
         }
         //raycast with box, check if tag, else have a question mark appear
         //simple tags on breakable objects/ocean spots
@@ -31,5 +31,31 @@ public class toolUse : BasicMovement
     void FixedUpdate()
     {
         //same thing with jump, but make it tool use with tag "breakable"
+    }
+    public bool IsFacingBreakable()
+    {
+        float lastPositionOffsetX = move.lastMovementDirection.x * move.breakableCheckOffsetX;
+        float lastPositionOffsetY = move.lastMovementDirection.y * move.breakableCheckOffsetY;
+        //Debug.Log(move.lastMovementDirection.x + " " + move.lastMovementDirection.y);
+        Vector2 rayStart = new Vector2((transform.position.x + lastPositionOffsetX), (transform.position.y + lastPositionOffsetY));    
+        
+        RaycastHit2D hit = Physics2D.BoxCast(rayStart, move.boxSize, 0, move.lastMovementDirection, move.breakableCheckDistance);
+
+        bool grounded = hit.collider != null;
+        
+        Debug.DrawRay(rayStart, move.lastMovementDirection*move.breakableCheckDistance, grounded ? Color.blue: Color.red);
+        
+        if(grounded)
+        {
+            //Debug.Log(hit.collider.tag);
+            grounded = hit.collider.tag != "Player";
+            if(grounded) {Debug.DrawRay(rayStart, move.lastMovementDirection*move.breakableCheckDistance,Color.red);}
+        }
+        else
+        {
+            Debug.DrawRay(rayStart, move.lastMovementDirection*move.breakableCheckDistance, Color.green);
+        }
+        
+        return grounded;
     }
 }
